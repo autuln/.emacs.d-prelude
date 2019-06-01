886a611524c90579c4f2b04c0544eb0283f61d57
autuln <autuln@gmail.com>
2019-06-02 03:08:32 +0900

    add comment

--- personal/custom/package/package-org-mode.el
+++ personal/custom/package/package-org-mode.el
@@ -12,3 +12,15 @@
 ;; #-*- mode: org -*-
 ;; #+STARTUP: showall
 ;; #+TODO: TODO IN-PROGRESS WAITING DONE
+
+(setq org-tag-alist '(("@OFFICE" . ?o)
+                      ("@HOME"   . ?h)
+                      ("@DEV"    . ?d)))
+;; or add org file top
+;; #+TAGS: @OFFICE(o) @HOME(h) @DEV(d)
+;; etc example)
+;; #+TAGS: { @OFFICE(o) @HOME(h) } COMPUTER(c) PHONE(p) READING(r)
+;; C-c -c : 태그 추가
+
+
+;; C-c C-e : 내보내기 메뉴
