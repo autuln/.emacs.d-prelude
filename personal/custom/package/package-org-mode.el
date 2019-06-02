06249fb3c465e8156857c2e2693b7124db255740
autuln <autuln@gmail.com>
2019-06-03 02:51:08 +0900

    org mode config

--- personal/custom/package/package-org-mode.el
+++ personal/custom/package/package-org-mode.el
@@ -5,7 +5,9 @@
 ;; group: Programming -> Tools -> org
 
 (setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "TODO(t)" "IN-PROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")
+        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")))
 ;; or add org file top
 ;; #+TODO: TODO IN-PROGRESS WAITING DONE
 ;; or add org file top
@@ -15,12 +17,28 @@
 
 (setq org-tag-alist '(("@OFFICE" . ?o)
                       ("@HOME"   . ?h)
-                      ("@DEV"    . ?d)))
+                      ("DEV"     . ?d)))
 ;; or add org file top
 ;; #+TAGS: @OFFICE(o) @HOME(h) @DEV(d)
 ;; etc example)
 ;; #+TAGS: { @OFFICE(o) @HOME(h) } COMPUTER(c) PHONE(p) READING(r)
+
+;; # shortcut
 ;; C-c -c : 태그 추가
-
-
 ;; C-c C-e : 내보내기 메뉴
+
+;; # command
+;; M-x org-reload : 업데이트 후 org-mode를 다시 불러오기
+;; M-x org-version : org-mode 버전 확인하기
+
+;; format
+;; #-*- mode: org -*-
+;; #+STARTUP: showall
+;; #+TITLE: Example .org file
+;; #+AUTHOR: autuln
+;; #+TAGS: { @OFFICE(o) @HOME(h) } COMPUTER(c) PHONE(p) READING(r)
+;; #+FILETAGS: :Peter:Boss:Secret:
+;; #+DATE: <2019-06-02 Sun>
+;; #+TODO: TODO(t) | DONE(d)
+;; #+TODO: REPORT(r) BUG(b) KNOWNCAUSE(k) | FIXED(f)
+;; #+TODO: | CANCELED(c)
